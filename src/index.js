579ad0d8a394239592f8d65c19dd85cd6a9a2a67
Joshua Richard Smith <veryjoshua@gmail.com>
2017-09-13 19:39:59 -0600

    changes to app

--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/app';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
-  </Provider>
   , document.querySelector('.container'));
